6fdd48a0a6e3b1b2e9e60135b249fea4ee4aa0c7
lev <programmwriter@mail.ru>
2020-04-26 14:02:21 +0500

    side-menu

--- src/js/btn-link.js
+++ src/js/btn-link.js
@@ -1,20 +1,34 @@
 // import  {toggleClass} from '../js/functions.js';
-import  {toggleClass} from '../js/functions';
+debugger;
+import { toggleClass } from "../js/functions";
 const btnLinks = document.querySelectorAll(".btn-link");
 const cont = document.querySelector(".container__side");
+const sideMenu = document.querySelector(".side-menu");
+const containerMain = document.querySelector(".container__main");
+const containerSide = document.querySelector(".container__side");
+const modalCall = document.querySelector(".call");
+const modalFeedback = document.querySelector(".feedback");
+
+cont.addEventListener("click", () => {
+  toggleClass(sideMenu, "side-menu--open");
+  toggleClass(sideMenu, "side-menu--hidden");
+  toggleClass(cont, "container__side--open");
+});
 
 btnLinks.forEach((el) => {
   el.addEventListener("click", (evt) => {
-    let sideMenu = document.querySelector(".side-menu");
-    let containerMain = document.querySelector(".container__main");   
-    let containerSide = document.querySelector(".container__side");   
-    let modalCall = document.querySelector(".call");
-    let modalFeedback = document.querySelector(".feedback");
     let targetData = evt.target.dataset.btnvalue;
 
     if (targetData === "burger") {
+      toggleClass(sideMenu, "side-menu--open");
+      sideMenu.classList.remove("side-menu--hidden");
+      toggleClass(cont, "container__side--open");
+    }
+    if (targetData === "burger-close") {
+      console.log(sideMenu);
+      toggleClass(sideMenu, "side-menu--open");
       toggleClass(sideMenu, "side-menu--hidden");
-      toggleClass(containerMain, "container__main--blur");
+      toggleClass(cont, "container__side--open");
     }
     if (targetData === "modall-call-open") {
       toggleClass(modalCall, "modal--hidden");
